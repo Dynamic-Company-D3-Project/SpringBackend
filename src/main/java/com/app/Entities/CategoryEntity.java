62236dfe0f57c0eb780d4124222a45ca15fbffeb
Arya Patil <arya07267@gmail.com>
2024-08-06 21:35:43 +0530

    Category

--- src/main/java/com/app/Entities/CategoryEntity.java
+++ src/main/java/com/app/Entities/CategoryEntity.java
@@ -1,13 +1,33 @@
 package com.app.Entities;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Builder
+@Getter
+@Setter
+
 public class CategoryEntity {
-@Id
+ @Id
  private Long id;
  private String name;
+ @Column(name = "description", columnDefinition = "TEXT")
  private String description;
  private String imageUrl;
+ 
+ @OneToMany(fetch = FetchType.LAZY, mappedBy = "categoryEntity", cascade = CascadeType.ALL)
+ private List<SubCategory> subCategoryList;
+ 
+ 
 }
